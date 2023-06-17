8b29b14956a5518e5b62c38b0615bcff4ddd7b9b
Roger Marques <marquesroger226@gmail.com>
2023-06-17 12:05:40 -0300

    refac: componentizing the banner image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,6 +2,7 @@
 
 import useHandleBanner from '@/hooks/useHandleBanner'
 import { Antonio } from 'next/font/google'
+import BannerImage from './BannerImage'
 
 const antonio = Antonio({
     weight: ['100', '300', '400'],
@@ -9,15 +10,10 @@ const antonio = Antonio({
 })
 
 export default function Banner() {
-    const {movie} = useHandleBanner()
+    const { movie } = useHandleBanner()
     return (
         <>
-            <section
-                style={{ backgroundImage: `url(${movie?.img})`, height: '500px' }}
-                className={`w-full h-[500px] bg-center bg-no-repeat bg-cover
-        absolute top-0 -z-10 brightness-50 shadow-[inset_120px_-200px_300px_-25px_#062635]`}
-            >
-            </section>
+            <BannerImage url={movie?.img} />
             <h1 id="teste" className={`${antonio.className} text-[35px] font-bold mt-[170px] min-[537px]:mt-[220px] text-white p-5`}>{movie.title}</h1>
         </>
     )
---
+++ src/components/BannerImage.tsx
@@ -0,0 +1,14 @@
+interface BannerImageProps {
+    url: string
+}
+
+export default function BannerImage({ url }: BannerImageProps) {
+    return (
+        <section
+            style={{ backgroundImage: `url(${url})`, height: '500px' }}
+            className={`w-full h-[500px] bg-center bg-no-repeat bg-cover
+        absolute top-0 -z-10 brightness-50 shadow-[inset_120px_-200px_300px_-25px_#062635]`}
+        >
+        </section>
+    )
+}
