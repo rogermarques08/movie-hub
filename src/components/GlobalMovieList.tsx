9488f5b26c582a63cfb2ce1d9eb6c6139d229f28
Roger Marques <marquesroger226@gmail.com>
2023-06-20 21:55:38 -0300

    feat: adding more lists

--- src/components/GlobalMovieList.tsx
+++ src/components/GlobalMovieList.tsx
@@ -3,9 +3,11 @@ import MovieList from "./MovieList"
 export default async function GlobalMovieList() {
     return (
         <div className="px-5 mt-[30px]">
-            <MovieList url="https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1" title="Filmes Populares" type="movie"/>
-            <MovieList url="https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=1" title="Mais Bem Avaliados" type="movie"/>
-            <MovieList url="https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1" title="Próximos Lançametos" type="movie"/>
+            <MovieList url="https://api.themoviedb.org/3/trending/movie/day?language=pt-BR" title="Populares Hoje" type="movie" />
+            <MovieList url="https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=1" title="Filmes Mais Bem Avaliados" type="movie" />
+            <MovieList url="https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1" title="Filmes Populares" type="movie" />
+            <MovieList url="https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1" title="Próximos Lançametos" type="movie" />
+            <MovieList url="https://api.themoviedb.org/3/tv/top_rated?language=pt-BR&page=1" title="Séries Mais Bem Avaliadas" type="tv" />
         </div>
     )
 }
