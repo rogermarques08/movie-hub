9a0c4ce278b5370918ca143831147a564e96a056
Roger Marques <marquesroger226@gmail.com>
2023-06-24 10:37:34 -0300

    fix: header resizing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 
 export default function Header() {
     return (
-        <header className='flex justify-center min-[537px]:justify-between gap-4 px-6 py-4 items-center flex-wrap'>
+        <header className='flex justify-center min-[625px]:justify-between gap-4 px-6 py-4 items-center flex-wrap'>
             <Logo />
             <Link href='/watch-list' className="font-semibold text-white">Assistir mais tarde</Link>
             <SearchInput />
