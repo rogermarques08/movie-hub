392809f2a34692c54fc3410529cc7344d9b32a88
Roger Marques <marquesroger226@gmail.com>
2023-06-26 17:01:21 -0300

    feat: adding animation on detail page

--- src/app/media/[...slug]/page.tsx
+++ src/app/media/[...slug]/page.tsx
@@ -8,14 +8,16 @@ import RecomendationList from "@/components/RecomendationList"
 export default async function MovieDetails({ params }: { params: { slug: string[] } }) {
     const slugs = params.slug
     const movie = await getData<FilmDataDetails>(`https://api.themoviedb.org/3/${slugs[0]}/${slugs[1]}?language=pt-BR`)
-    
+
     return (
         <main>
             <BannerImage url={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
-            <Details movie={movie} />
-            <MovieTrailer movieId={movie.id} />
-            <CastList url={`https://api.themoviedb.org/3/movie/${movie.id}/credits?language=pt-BR`} />
-            <RecomendationList url={`https://api.themoviedb.org/3/movie/${movie.id}/recommendations?language=en-US&page=1`} />
+            <div className="fade-intro">
+                <Details movie={movie} />
+                <MovieTrailer movieId={movie.id} />
+                <CastList url={`https://api.themoviedb.org/3/movie/${movie.id}/credits?language=pt-BR`} />
+                <RecomendationList url={`https://api.themoviedb.org/3/movie/${movie.id}/recommendations?language=en-US&page=1`} />
+            </div>
         </main>
     )
 }
